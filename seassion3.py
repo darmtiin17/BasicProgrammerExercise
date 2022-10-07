36b0caa7b7da7ac3d2510298bdcb92be38f5f0c0
darmtiin17 <114727612+darmtiin17@users.noreply.github.com>
2022-10-07 14:38:20 +0700

    session3

---
+++ seassion3.py
@@ -0,0 +1,22 @@
+#program created by Muhammad Haidar Matin
+#ti22i
+
+from operator import truediv
+
+
+name = "haidar"
+kelas = "ti22i"
+major = "informatics engineering"
+age = "21"
+quote = "Learn from yesterday and live for today, then hope for tomorrow"
+ilikeseblak = True
+hobbies = ["travelling, and jogging"]
+
+#display
+print("hello ! my name is :", name)
+print("i'm a student",kelas)
+print('majoring:', major)
+print("i'm", age, "year old")
+print("my motto :", quote)
+print("do i like seblak:", ilikeseblak)
+print("my hobbies are:", hobbies)
